b69d5fdc2c4cebdbe7df4304b20efdbc34826187
B3SUS <IB3SUSI@gmail.com>
2025-02-24 21:49:11 +0300

    v

--- src/app/listing/page.tsx
+++ src/app/listing/page.tsx
@@ -3,7 +3,6 @@ import {FilterCheckbox} from "@/components/shared/listing-page/FilterCheckbox";
 export default function Listing(){
     return(
         <div>
-            Listing
             <FilterCheckbox text={'text'} value={'value'}/>
         </div>
     )
