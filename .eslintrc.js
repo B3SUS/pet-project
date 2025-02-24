94d217347e3267281a3c033b33024471390fdcd4
B3SUS <IB3SUSI@gmail.com>
2025-02-24 21:11:50 +0300

    v

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-    extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+    extends: ["eslint:recommended", "plugin:react/recommended", "prettier", 'next'],
     rules: {
         "no-console": "off", // Разрешает console.log
         "react/prop-types": "off", // Отключает проверку PropTypes
         "react/react-in-jsx-scope": "off", // Не нужен в Next.js
-        "prettier/prettier": ["error"]
+        "prettier/prettier": ["error"],
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-page-custom-font': 'off',
     },
     "plugins": ["prettier"],
 };
